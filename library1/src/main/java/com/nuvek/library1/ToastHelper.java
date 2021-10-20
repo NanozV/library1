4c8c032047c74c22d2a1af617eddc5c61cfcc678
Ricardo Vera Vargas <ricardoveravargas@MacBook-Pro-de-Ricardo.local>
2021-10-20 10:52:58 -0500

    first commit

---
+++ library1/src/main/java/com/nuvek/library1/ToastHelper.java
@@ -0,0 +1,11 @@
+package com.nuvek.library1;
+
+import android.util.Log;
+
+public class ToastHelper {
+    static final String TAG = "nanoz_test";
+
+    public static void showMessaje(String messaje) {
+        Log.d(TAG, messaje);
+    }
+}
